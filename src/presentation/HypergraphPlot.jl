c2aabb620f75f2c4cd5b4d5b4d3a0522211b560e
Anastasia Golovin <asgolovin@protonmail.com>
2022-07-21 19:36:16 +0200

    Plot hyperedges of any size

--- src/presentation/HypergraphPlot.jl
+++ src/presentation/HypergraphPlot.jl
@@ -1,10 +1,12 @@
 using GLMakie
+using GeometryBasics
+using Combinatorics
 
 include("../data/Network.jl")
 
 @recipe(HypergraphPlot) do scene
     Attributes(
-        S_color = :seagreen,
+        S_color = :yellowgreen,
         I_color = :firebrick2,
         triangle_color = (:orange, 0.3),
         hyperedge_colormap = :GnBu
@@ -13,8 +15,6 @@ end
 
 function Makie.plot!(hgplot::HypergraphPlot)
     network = hgplot[1]
-
-    # define plot elements that can change with time as observables
 
     # a vector of states expressed as integers 
     states = Observable(Int64[])
@@ -25,7 +25,9 @@ function Makie.plot!(hgplot::HypergraphPlot)
     # positions of the nodes in the image
     node_pos = Observable(Point{2, Float32}[])
     # indices of the nodes which belong to the same triangle
-    faces = Observable(Int64[])
+    faces = Observable(Matrix{Int64}(undef, 0, 3))
+    # for each face stores the size of the hyperedge to which the face belongs to
+    hyperedge_size = Observable(Int64[])
     
     # Called whenever the network changes to update all observables with new values
     # TODO: why is it called twice?
@@ -34,19 +36,27 @@ function Makie.plot!(hgplot::HypergraphPlot)
         
         empty!(states[])
         empty!(labels[])
-        faces[] = []
+        faces[] = Matrix{Int64}(undef, 0, 3)
+        empty!(hyperedge_size[])
         for node = 1:get_num_nodes(network)
             push!(states[], Int(get_state(network, node)))
             push!(labels[], "#$node")
         end
         for h = 1:get_num_hyperedges(network)
-            if get_hyperedge_size(network, h) > 2
+            hsize = get_hyperedge_size(network, h)
+            if hsize > 2
                 nodes = get_nodes(network, h)
-                faces[] = [faces[]; nodes]
+                # add triangular faces between all triples of nodes
+                simplex_faces = collect(combinations(nodes, 3))
+                simplex_faces = reduce(vcat, transpose.(simplex_faces))
+                faces[] = [faces[]; simplex_faces]
+                push!(hyperedge_size[], hsize)
             end
         end
+        # trigger the update of observables
         states[] = states[]
         labels[] = labels[]
+        hyperedge_size[] = hyperedge_size[]
     end
 
     # call update_plot whenever the network changes
@@ -56,8 +66,8 @@ function Makie.plot!(hgplot::HypergraphPlot)
     update_plot(network[])
 
     # map states to colors
-    colormap = Observable{Any}()
-    map!(colormap, hgplot.S_color, hgplot.I_color) do I_color, S_color
+    node_colormap = Observable{Any}()
+    map!(node_colormap, hgplot.S_color, hgplot.I_color) do I_color, S_color
         [I_color, S_color]
     end
 
@@ -66,21 +76,26 @@ function Makie.plot!(hgplot::HypergraphPlot)
                     simple_graph; 
                     nlabels=labels,
                     nlabels_distance=10,
-                    node_attr = (color=states, colormap=colormap, markersize=15),
+                    node_attr = (color=states, colormap=node_colormap, markersize=15),
                     nlabels_attr = (textsize = 12, color = :gray))
     
     # graphplot gives us the positions of the nodes
     node_pos = gp[:node_pos]
 
     # plot the hyperedges as triangles
-    mesh!(hgplot, node_pos, faces, color = hgplot.triangle_color, shading = false)
+    mesh!(hgplot, 
+          node_pos,
+          faces,
+          color = (:orange, 0.3),
+          #colormap = hgplot.hyperedge_colormap,
+          shading = false)
 
     return hgplot
 end
 
 n = 10
 network = Observable(HyperNetwork(n, 0.3))
-build_RSC_hg!(network[], (20, 5))
+build_RSC_hg!(network[], (10, 2, 1))
 
 f = Figure()
 display(f)
