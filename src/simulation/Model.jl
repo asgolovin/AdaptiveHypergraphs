25f632c90c2ed596daeeda4890c43085c2a27f11
Anastasia Golovin <asgolovin@protonmail.com>
2022-08-13 11:15:02 +0200

    First structure for the continuous model

--- src/simulation/Model.jl
+++ src/simulation/Model.jl
@@ -15,16 +15,6 @@ struct DiscrModel{P <: PropagationRule, A <: AdaptivityRule} <: AbstractModel{P,
 end
 
 
-struct ContinuousModel{P <: PropagationRule, A <: AdaptivityRule} <: AbstractModel{P, A}
-    network::HyperNetwork
-    event_queue::PriorityQueue
-    propagation_rule::PropagationRule
-    adaptivity_rule::AdaptivityRule
-    propagation_rate::Real
-    adaptivity_rate::Real
-end
-
-
 """
 Advances the dynamics of the network by one step. 
 
@@ -54,6 +44,47 @@ function step!(model::DiscrModel)
 
     return true
 end
+
+
+struct ContinuousModel{P <: PropagationRule, A <: AdaptivityRule} <: AbstractModel{P, A}
+    network::HyperNetwork
+    event_queue::PriorityQueue
+    propagation_rule::PropagationRule
+    adaptivity_rule::AdaptivityRule
+    propagation_rate::Real
+    adaptivity_rate::Real
+
+    function ContinuousModel(network::HyperNetwork,
+                             propagation_rule::PropagationRule,
+                             adaptivity_rule::AdaptivityRule, 
+                             propagation_rate::Real,
+                             adaptivity_rate::Real)
+        event_queue = PriorityQueue()
+        he_meta = (:change_of_status_time => Inf)
+
+        for hyperedge in 1:get_num_hyperedges(network)
+            set_hyperedge_meta!(network, hyperedge, he_meta)
+            if is_active(network, hyperedge)
+                event = Event()
+                set_hyperedge_meta!(network, hyperedge, :change_of_status_time, 0.)
+                enqueue!(event_queue, event, 0)
+            end
+        end
+
+        ContinuousModel(network,
+                        event_queue,
+                        propagation_rule,
+                        adaptivity_rule,
+                        propagation_rate,
+                        adaptivity_rate)
+    end
+end
+
+
+struct Event
+    
+end
+
 
 # TODO
 function step!(model::ContinuousModel)
