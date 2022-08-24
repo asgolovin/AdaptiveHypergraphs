c15606cc90aff35084262f8a83ed018ac9adfaa9
Anastasia Golovin <asgolovin@protonmail.com>
2022-08-24 11:12:38 +0200

    record_history takes only the ModelObservable

--- src/presentation/ModelObservable.jl
+++ src/presentation/ModelObservable.jl
@@ -9,24 +9,24 @@ struct ModelObservable{M<:AbstractModel}
     state_history::Dict{State,Observable{Vector{Int64}}}
     hyperedge_history::Dict{Int64,Observable{Vector{Int64}}}
     active_hyperedges_history::Observable{Vector{Int64}}
-end
 
-function ModelObservable{M}(model::M) where {M<:AbstractModel}
-    state_history = Dict{State,Observable{Vector{Int64}}}()
-    hyperedge_history = Dict{Int64,Observable{Vector{Int64}}}()
-    active_hyperedges_history = Observable(Vector{Int64}())
-    for state in instances(State)
-        state_history[state] = Observable(Vector{Int64}())
+    function ModelObservable{M}(model::M) where {M<:AbstractModel}
+        state_history = Dict{State,Observable{Vector{Int64}}}()
+        hyperedge_history = Dict{Int64,Observable{Vector{Int64}}}()
+        active_hyperedges_history = Observable(Vector{Int64}())
+        for state in instances(State)
+            state_history[state] = Observable(Vector{Int64}())
+        end
+        for size in 2:get_max_hyperedge_size(model.network)
+            hyperedge_history[size] = Observable(Vector{Int64}())
+        end
+        mo = new(Observable(model),
+                 Observable(model.network),
+                 state_history,
+                 hyperedge_history,
+                 active_hyperedges_history)
+        return record_history!(mo)
     end
-    for size in 2:get_max_hyperedge_size(model.network)
-        hyperedge_history[size] = Observable(Vector{Int64}())
-    end
-    record_history!(model, state_history, hyperedge_history, active_hyperedges_history)
-    return ModelObservable(Observable(model),
-                           Observable(model.network),
-                           state_history,
-                           hyperedge_history,
-                           active_hyperedges_history)
 end
 
 """
@@ -37,40 +37,39 @@ Progress the model one time step forward and update the history.
 function step!(mo::ModelObservable)
     network_changed = step!(mo.model[])
     network_changed && notify(mo.model)
-    record_history!(mo.model[], mo.state_history, mo.hyperedge_history,
-                    mo.active_hyperedges_history)
-    return mo
+    return record_history!(mo)
 end
 
-# TODO: only take model observable as input
 """
-    record_history!(model::AbstractModel, state_history::Dict{State, Observable{Vector{Int64}}}, hyperedge_history::Dict{Int64, Observable{Vector{Int64}}})
+    record_history!(mo::ModelObservable)
 
 Pushes the current distribution of states from the model into state_history.
 """
-function record_history!(model::AbstractModel,
-                         state_history::Dict{State,Observable{Vector{Int64}}},
-                         hyperedge_history::Dict{Int64,Observable{Vector{Int64}}},
-                         active_hyperedges_history::Observable{Vector{Int64}})
-    state_dist = get_state_dist(model.network)
+function record_history!(mo::ModelObservable)
+    state_history = mo.state_history
+    hyperedge_history = mo.hyperedge_history
+    active_hyperedges_history = mo.active_hyperedges_history
+    network = mo.network[]
+    state_dist = get_state_dist(network)
+
     for state in keys(state_dist)
         push!(state_history[state][], state_dist[state])
         notify(state_history[state])
     end
-    hyperedge_dist = get_hyperedge_dist(model.network)
+    hyperedge_dist = get_hyperedge_dist(network)
     for size in keys(hyperedge_dist)
         push!(hyperedge_history[size][], hyperedge_dist[size])
         notify(hyperedge_history[size])
     end
     active_count = 0
-    for hyperedge in get_hyperedges(model.network)
-        if is_active(model.network, hyperedge)
+    for hyperedge in get_hyperedges(network)
+        if is_active(network, hyperedge)
             active_count += 1
         end
     end
     push!(active_hyperedges_history[], active_count)
     notify(active_hyperedges_history)
-    return nothing
+    return mo
 end
 
 """
@@ -82,9 +81,7 @@ function rebind_model!(mo::ModelObservable, model::AbstractModel)
     clear!(mo)
     mo.model[] = model
     mo.network[] = model.network
-    record_history!(model, mo.state_history, mo.hyperedge_history,
-                    mo.active_hyperedges_history)
-    return mo
+    return record_history!(mo)
 end
 
 """
