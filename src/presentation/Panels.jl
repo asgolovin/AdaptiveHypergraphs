131ecba6c5518023083b51071a012311271845bc
Anastasia Golovin <asgolovin@protonmail.com>
2022-09-16 15:05:53 +0200

    Fix errors

--- src/presentation/Panels.jl
+++ src/presentation/Panels.jl
@@ -144,8 +144,11 @@ end
 
 mutable struct SlowManifoldPanel <: AbstractPanel
     time_series::Vector{AbstractTimeSeries}
-    axes::Axis
+    axes::Vector{Axis}
     lines::Vector{Lines}
+    num_subplots::Int64
+    num_cols::Int64
+    num_rows::Int64
     xlow::Union{Real,Nothing}
     xhigh::Union{Real,Nothing}
     ylow::Union{Real,Nothing}
@@ -156,15 +159,28 @@ function SlowManifoldPanel(box::GridSubposition, mo::ModelObservable;
                            xlow=nothing, xhigh=nothing,
                            ylow=nothing, yhigh=nothing)
     lines = []
+    axes = []
     title = "Slow manifold plot"
-    ax = Axis(box[1, 1]; title=title)
-    l = lines!(ax,
-               mo.state_series[1].observable,
-               mo.active_hyperedges_series.observable)
-    xlims!(ax; low=xlow, high=xhigh)
-    ylims!(ax; low=ylow, high=yhigh)
-    push!(lines, l)
-    return SlowManifoldPanel([mo.state_series[1], mo.active_hyperedges_series], ax, lines,
+    num_subplots = get_max_hyperedge_size(mo.network[]) - 1
+    num_cols = 2
+    num_rows = Int64(ceil(num_subplots / num_cols))
+    for i in 1:num_subplots
+        col = mod1(i, num_cols)
+        row = (i - 1) ÷ num_cols + 1
+        ax = Axis(box[row, col]; title=title)
+        l = lines!(ax,
+                   mo.state_series[1].observable,
+                   mo.active_hyperedges_series[i].observable)
+        xlims!(ax; low=xlow, high=xhigh)
+        ylims!(ax; low=ylow, high=yhigh)
+        push!(lines, l)
+        push!(axes, ax)
+    end
+    all_series = vcat([mo.state_series[1]], mo.active_hyperedges_series)
+    return SlowManifoldPanel(all_series,
+                             axes,
+                             lines,
+                             num_subplots, num_cols, num_rows,
                              xlow, xhigh, ylow, yhigh)
 end
 
@@ -219,11 +235,13 @@ function FinalMagnetizationPanel(box::GridSubposition, mo::ModelObservable;
 end
 
 function deactivate_lines!(panel::SlowManifoldPanel)
-    lines!(panel.axes,
-           panel.time_series[1].observable[],
-           panel.time_series[2].observable[];
-           linewidth=1,
-           color=(:gray, 0.5))
+    for i in 1:(panel.num_subplots)
+        lines!(panel.axes[i],
+               panel.time_series[1].observable[],
+               panel.time_series[i + 1].observable[];
+               linewidth=1,
+               color=(:gray, 0.5))
+    end
     # Bring the lines tied to observables in front of the gray lines
     for line in panel.lines
         translate!(line, 0, 0, 1)
@@ -241,6 +259,14 @@ function deactivate_lines!(panel::AbstractTimeSeriesPanel)
     # Bring the lines tied to observables in front of the gray lines
     for line in panel.lines
         translate!(line, 0, 0, 1)
+    end
+    return panel
+end
+
+function set_lims!(panel::SlowManifoldPanel)
+    for i in 1:(panel.num_subplots)
+        xlims!(panel.axes[i]; low=panel.xlow, high=panel.xhigh)
+        ylims!(panel.axes[i]; low=panel.ylow, high=panel.yhigh)
     end
     return panel
 end
