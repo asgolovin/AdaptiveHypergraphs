78f9253e4c86fbf414616a750e0fef03ffd7c55a
Anastasia Golovin <asgolovin@protonmail.com>
2022-11-15 18:24:29 +0100

    Change params

--- input/large_network.jl
+++ input/large_network.jl
@@ -11,13 +11,13 @@ params = InputParams(
         num_time_steps = Int64(1e6),
         adaptivity_rule = RewireToRandom(),
         propagation_rule = ProportionalVoting(),
-        adaptivity_prob = [0.1, 0.8]
+        adaptivity_prob = 0.5
     ),
     VisualizationParams(
         skip_points = 100,
         buffer_size = 100000
     ),
     BatchParams(
-        batch_size = 3
+        batch_size = 10
     )
 )
