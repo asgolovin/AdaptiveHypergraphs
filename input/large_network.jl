0fbc6780cdda136e3b9136298a65479c6f68cb39
Anastasia Golovin <asgolovin@protonmail.com>
2022-12-12 15:42:23 +0100

    Run the simulation for very long

--- input/large_network.jl
+++ input/large_network.jl
@@ -5,19 +5,20 @@ using AdaptiveHypergraphs
 params = InputParams(
     NetworkParams(
         num_nodes = 1000,
-        num_hyperedges = (2000, 200, 20)
+        num_hyperedges = (2000, 200, 20),
+        infected_prob = 0.5
     ),
     ModelParams(
-        num_time_steps = Int64(1e6),
+        num_time_steps = Int64(1e8),
         adaptivity_rule = RewireToRandom(),
         propagation_rule = ProportionalVoting(),
-        adaptivity_prob = 0.5
+        adaptivity_prob = collect(0.1:0.1:0.9)
     ),
     VisualizationParams(
         skip_points = 100,
         buffer_size = 100000
     ),
     BatchParams(
-        batch_size = 10
+        batch_size = 1
     )
 )
